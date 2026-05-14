5e625be0b97ab0979741537b724d870b19f8c94f
Akaba <r.akaba@lancaster.ac.uk>
2026-05-14 13:57:51 +0200

    feat: added js but will upgrade it

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+   
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(link => {  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  }
+});
+getElementById("themeButton").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+});
+const date = new Date();
+document.getElementById("current-date").textContent = date.toLocaleDateString('en-GB', { 
+    weekday: 'long', 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric' 
+});
+});
